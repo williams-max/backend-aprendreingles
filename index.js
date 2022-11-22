aa390c498c79ee521a6c1b247548855eff54b505
Israel <almanzaisrael75@gmail.com>
2022-11-22 14:24:04 -0400

    agregando apis para traducir

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const app = express();
+const server = express();
+const bodyParser = require("body-parser");
+const cors = require('cors');
 
-const port=process.env.PORT || 4000;
-app.listen(port);
+const PORT=process.env.PORT || 4000;
 
-app.get("/",(req,res)=> {
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+server.use(cors({
+  origin: '*'
+}));
+
+const showRoutes = require("./routes/index.js");
+
+server.use("/api", showRoutes(server));
+
+
+server.get("/",(req,res)=> {
     res.send("backend funcionando");
 })
 
-console.log(`listen on Port ${port}`)
+server.listen(PORT, err => {
+    if (err) throw err;
+    console.log(`> Ready on ${PORT}`);
+});
