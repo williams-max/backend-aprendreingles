f742e6f41da5342d89b792e8470bc13559aefcf5
Israel <almanzaisrael75@gmail.com>
2022-11-22 13:56:13 -0400

    update add demo aprende ingles back v12

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const app = express();
 const port=process.env.PORT || 4000;
 app.listen(port);
 
+app.get("./",(req,res)=> {
+    res.send("backend funcionando");
+})
+
 console.log(`listen on Port ${port}`)
