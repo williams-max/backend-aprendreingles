5df073fc874332196277ba1c7d81b8f33d7bcab0
Israel <almanzaisrael75@gmail.com>
2022-11-23 16:12:54 -0400

    manejo de errores

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const bodyParser = require("body-parser");
 const cors = require('cors');
 const { translate } = require('free-translate');
 
-var texto = " ";
+//var texto = " ";
 
-const PORT=process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
 server.use(cors({
-  origin: '*'
+    origin: '*'
 }));
 
 const showRoutes = require("./routes/index.js");
@@ -20,17 +20,46 @@ const showRoutes = require("./routes/index.js");
 //server.use("/api", showRoutes(server));
 
 server.get('/', async (req, res) => {
-    texto="hola"
-    const textEnglish = await translate(texto, { to: 'en' });
-    res.send(textEnglish);
+
+    try {
+        texto = "hola"
+        const textEnglish = await translate(texto, { to: 'en' });
+        res.send(textEnglish);
+    } catch (err) {
+        //
+        console.log(err)
+        var aux=err.toString();
+        console.log("error ",aux)
+
+        res.send(aux);
+     //   JSON.stringify(JSON.parse(jsonStr));
+     //   res.send(JSON.stringify(JSON.parse(err)))
+      //  next(err);
+    }
 });
 /*
 server.get("/",(req,res)=> {
     res.send("backend funcionando");
 })*/
-server.get("/api/hello",(req,res)=> {
-    res.send("hello funcionando");
+server.get("/api/hello", (req, res, next) => {
+
+    try {
+        res.send("hello funcionando");
+        // throw new Error('There was an error getting the users');
+
+    } catch (err) {
+        next(err);
+    }
 })
+
+function handleErrors(err, req, res, next) {
+    console.log(err);
+
+ //   res.send(err)
+    res.status(500).send('An internal server error occurred');
+};
+
+//server.use(handleErrors);
 
 server.listen(PORT, err => {
     if (err) throw err;
